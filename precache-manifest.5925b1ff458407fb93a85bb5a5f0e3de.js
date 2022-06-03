e26c7be322ace3feab2c0b45eca652f56964cf5d
Mari <Masianuy91@gmail.com>
2022-06-03 17:27:41 +0300

    Updates

---
+++ precache-manifest.5925b1ff458407fb93a85bb5a5f0e3de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d59fb359fc07fb3c7bab",
+    "url": "/traveling/static/css/main.db3ca290.chunk.css"
+  },
+  {
+    "revision": "d59fb359fc07fb3c7bab",
+    "url": "/traveling/static/js/main.d59fb359.chunk.js"
+  },
+  {
+    "revision": "5d9ee79056c3254d4697",
+    "url": "/traveling/static/js/1.5d9ee790.chunk.js"
+  },
+  {
+    "revision": "f075425d380cb36eb463",
+    "url": "/traveling/static/js/runtime~main.f075425d.js"
+  },
+  {
+    "revision": "804e65d3b6de88b0b8d74bb658696726",
+    "url": "/traveling/static/media/Pexels Videos 2109902.804e65d3.mp4"
+  },
+  {
+    "revision": "5d9b2e0f3065c959f424e716603a5125",
+    "url": "/traveling/static/media/pexels-spencer-davis-4388292.5d9b2e0f.jpg"
+  },
+  {
+    "revision": "33ee6186eeeab6b198c62391caa99174",
+    "url": "/traveling/static/media/fabio.33ee6186.jpg"
+  },
+  {
+    "revision": "dba9e9739840118f6bf0bf35a96e7d5c",
+    "url": "/traveling/static/media/unsplash-min.dba9e973.jpg"
+  },
+  {
+    "revision": "485d2e196a4e0bc092707876f5f1ce8e",
+    "url": "/traveling/index.html"
+  }
+];
